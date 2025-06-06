06730c37812ee41d0769650b9b078c6873780d0d
Хлопкова Полина <polina.polinad.davydova@mail.ru>
2025-06-06 17:20:19 +0700

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -7,15 +7,3 @@ def mask_account_number(number: str) -> str:
     return number[:6] + '*' * (len(number) - 10) + number[-4:]
 
 def mask_account_card(info: str) -> str:
-    '''Разделяем строку по последнему пробелу (имя типа может иметь пробелы)'''
-    last_space = info.rfind(' ')
-    name = info[:last_space]
-    number = info[last_space + 1:]
-
-    '''Определяем тип (если начинается с "Счет", используем маску для счета)'''
-    if name.startswith('Счет'):
-        masked_number = mask_account_number(number)
-    else:
-        masked_number = mask_card_number(number)
-
-    return f"{name} {masked_number}"
