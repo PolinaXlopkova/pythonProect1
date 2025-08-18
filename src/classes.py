b994e49c284cff0fbf2fcd13173a106b906ffd2b
Хлопкова Полина <polina.polinad.davydova@mail.ru>
2025-08-18 15:17:37 +0700

    classes.py

--- src/classes.py
+++ src/classes.py
@@ -1,9 +1,21 @@
 class Product:
     def __init__(self, name, price, description, quantity):
         self.name = name
-        self.price = price
+        self.__price = price
         self.description = description
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print ("Цена не должна быть нулевая или отрицательная")
+
 
 class Category:
     product_count = 0
@@ -12,5 +24,23 @@ class Category:
     def __init__(self, name, quantity):
         self.name = name
         self.quantity = quantity
-        self.products = []
+        self.__products = []
         Category.category_count += 1
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        return ''.join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n" for product in self.__products])
+
+    @classmethod
+    def new_product(cls, product_data):
+        return Product(
+            name=product_data['name'],
+            price=product_data['price'],
+            description=product_data['description'],
+            quantity=product_data['quantity']
+        )
